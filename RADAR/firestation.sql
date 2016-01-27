7b52d24e1a9103f4ab4d90addc145b7c65700cab
ndondeti <vdondeti@asu.edu>
2016-01-27 12:39:34 -0700

    Push

--- RADAR/firestation.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS 'predicates_for_fireStation';
-
-CREATE TABLE predicates_for_fireStation (id int, description varchar(50));
-
-insert into predicates_for_fireStation values (1, 'has_small_engines_number'), (2, 'has_big_engines_number'), (3, 'has_ladders_number'), (4, 'has_bulldozers_number'), (5, 'has_helicopters_number'), (6, 'has_rescuers_number');
--- RADAR/predicates_police.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS 'predicates_for_policeStation';
-
-CREATE TABLE predicates_for_policeStation (id int, description varchar(50));
-
-insert into predicates_for_policeStation values (1, 'has_police_car_number'), (2, 'has_policemen_number');
